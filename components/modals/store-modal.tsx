dacc9ae7a158404cd10b10b7bf122e5a7d0ce573
m.pylypchuk <nikolay.pilipchuk@gmail.com>
2023-12-17 21:14:01 +0200

    ECA-04 add dashboard page with ability to get store data

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -18,6 +18,7 @@ import {
 }                                   from "@/components/ui/form"
 import { Input }                    from "@/components/ui/input"
 import { Button }                   from "@/components/ui/button";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
     name: z.string().min(1)
@@ -41,9 +42,9 @@ export const StoreModal = () => {
 
             const response = await axios.post('/api/stores', values)
 
-            console.log(response.data);
+            window.location.assign(`/${response.data.id}`)
         } catch (error) {
-            console.log(error)
+            toast.error("Something went wrong.")
         } finally {
             setLoading(false)
         }
