6fbb3460d699ecb0be036bba16d9459b4654935e
m.pylypchuk <nikolay.pilipchuk@gmail.com>
2024-01-01 14:44:25 +0200

    ECA-10 clean up code

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -42,7 +42,7 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
             <AlertDescription className="mt-4 flex items-center justify-between">
                 <code className="relative rounded bg-muted px-[0.3rem] pt-[0.2rem] font-mono text-sm font-semibold">
                     {description}
-                    <Button variant="outline" size="icon" onClick={() => onCopy(description)}>
+                    <Button variant="outline" size="icon" onClick={onCopy}>
                         <Copy className="h-4 w-4"/>
                     </Button>
                 </code>
